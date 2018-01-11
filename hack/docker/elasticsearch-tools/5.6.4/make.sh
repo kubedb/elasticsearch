8c496d95ad7e033720d2c38dfc59040127ee5a87
shahriar <aerofoil.kite@gmail.com>
2018-01-11 12:40:26 +0600

    added-all

--- hack/docker/elasticsearch-tools/5.6.4/make.sh
+++ hack/docker/elasticsearch-tools/5.6.4/make.sh
@@ -19,14 +19,14 @@ build() {
     pushd "$REPO_ROOT/hack/docker/elasticsearch-tools/$TAG"
 
     # Download osm
-    #wget https://cdn.appscode.com/binaries/osm/${OSM_VER}/osm-alpine-amd64
-    #chmod +x osm-alpine-amd64
-    #mv osm-alpine-amd64 osm
+    wget https://cdn.appscode.com/binaries/osm/${OSM_VER}/osm-alpine-amd64
+    chmod +x osm-alpine-amd64
+    mv osm-alpine-amd64 osm
 
     local cmd="docker build -t $DOCKER_REGISTRY/$IMG:$TAG ."
     echo $cmd; $cmd
 
-    #rm osm
+    rm osm
     popd
 }
 
