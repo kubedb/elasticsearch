0cced38d817f5ae509cc8451c7f66d2cd6f104ef
tahsinrahman <tahsynx@gmail.com>
2018-05-18 11:23:36 +0600

    use gke

--- hack/concourse/test.sh
+++ hack/concourse/test.sh
@@ -7,30 +7,22 @@ entrypoint.sh
 docker login --username=$DOCKER_USER --password=$DOCKER_PASS
 docker run hello-world
 
-#install python pip
+# install python pip
 apt-get update > /dev/null
 apt-get install -y python python-pip > /dev/null
 
-#install kubectl
+# install kubectl
 curl -LO https://storage.googleapis.com/kubernetes-release/release/$(curl -s https://storage.googleapis.com/kubernetes-release/release/stable.txt)/bin/linux/amd64/kubectl &> /dev/null
 chmod +x ./kubectl
 mv ./kubectl /bin/kubectl
 
-#install onessl
+# install onessl
 curl -fsSL -o onessl https://github.com/kubepack/onessl/releases/download/0.3.0/onessl-linux-amd64 \
   && chmod +x onessl \
   && mv onessl /usr/local/bin/
 
-#install pharmer
-mkdir -p $GOPATH/src/github.com/pharmer
-pushd $GOPATH/src/github.com/pharmer
-git clone https://github.com/pharmer/pharmer &> /dev/null
-cd pharmer
-go get -u golang.org/x/tools/cmd/goimports
-./hack/builddeps.sh &> /dev/null
-./hack/make.py &> /dev/null
-pharmer
-popd
+# install pharmer
+go get -u github.com/pharmer/pharmer
 
 function cleanup {
     # delete cluster on exit
@@ -54,18 +46,21 @@ pushd elasticsearch
 NAME=elasticsearch-$(git rev-parse --short HEAD)
 popd
 
-#create credential file for pharmer
-cat > cred.json <<EOF
-{
-        "token" : "$TOKEN"
-}
-EOF
+#copy elasticsearch to $GOPATH
+mkdir -p $GOPATH/src/github.com/kubedb
+cp -r elasticsearch $GOPATH/src/github.com/kubedb
+pushd $GOPATH/src/github.com/kubedb/elasticsearch
+
+./hack/builddeps.sh
+export APPSCODE_ENV=dev
+export DOCKER_REGISTRY=kubedbci
+./hack/docker/es-operator/make.sh build
+./hack/docker/es-operator/make.sh push
+popd
 
 #create cluster using pharmer
-#note: make sure the zone supports volumes, not all regions support that
-#"We're sorry! Volumes are not available for Droplets on legacy hardware in the NYC3 region"
-pharmer create credential --from-file=cred.json --provider=DigitalOcean cred
-pharmer create cluster $NAME --provider=digitalocean --zone=nyc1 --nodes=2gb=1 --credential-uid=cred --kubernetes-version=v1.10.0
+pharmer create credential --from-file=creds/gcs/gke.json --provider=GoogleCloud cred
+pharmer create cluster $NAME --provider=gke gk10 --zone=us-central1-f --nodes=n1-standard-1=1 --credential-uid=cred --kubernetes-version=1.9.7-gke.0
 pharmer apply $NAME
 pharmer use cluster $NAME
 #wait for cluster to be ready
@@ -90,9 +85,6 @@ kubectl get storageclass
 
 export CRED_DIR=$(pwd)/creds/gcs/gcs.json
 
-#copy elasticsearch to $GOPATH
-mkdir -p $GOPATH/src/github.com/kubedb
-cp -r elasticsearch $GOPATH/src/github.com/kubedb
 pushd $GOPATH/src/github.com/kubedb/elasticsearch
 
 # create config/.env file that have all necessary creds
@@ -106,10 +98,17 @@ GOOGLE_APPLICATION_CREDENTIALS=$CRED_DIR
 AZURE_ACCOUNT_NAME=$AZURE_ACCOUNT_NAME
 AZURE_ACCOUNT_KEY=$AZURE_ACCOUNT_KEY
 
+OS_AUTH_URL=$OS_AUTH_URL
+OS_TENANT_ID=$OS_TENANT_ID
+OS_TENANT_NAME=$OS_TENANT_NAME
+OS_USERNAME=$OS_USERNAME
+OS_PASSWORD=$OS_PASSWORD
+OS_REGION_NAME=$OS_REGION_NAME
 
 S3_BUCKET_NAME=$S3_BUCKET_NAME
 GCS_BUCKET_NAME=$GCS_BUCKET_NAME
 AZURE_CONTAINER_NAME=$AZURE_CONTAINER_NAME
+SWIFT_CONTAINER_NAME=$SWIFT_CONTAINER_NAME
 EOF
 
 
