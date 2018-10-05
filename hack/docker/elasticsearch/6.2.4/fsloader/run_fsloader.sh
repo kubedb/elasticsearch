5b680d52cfd497067e4f54b3fcca90134c118880
emruz-hossain <emruz@appscode.com>
2018-10-05 16:36:06 +0600

    add exec before tail

--- hack/docker/elasticsearch/6.2.4/fsloader/run_fsloader.sh
+++ hack/docker/elasticsearch/6.2.4/fsloader/run_fsloader.sh
@@ -11,17 +11,17 @@ sync
 if [ "$SEARCHGUARD_DISABLED" == false ]; then
   case "$MODE" in
     client)
-    # Run sgadmin in client node (with ordinal 0 only)
+      # Run sgadmin in client node (with ordinal 0 only)
       ordinal="${NODE_NAME##*-}"
       if [ "$ordinal" == "0" ]; then
         /fsloader/run_sgadmin.sh
         exec /fsloader/fsloader run --mount-file "$searchguard"/sgconfig/sg_internal_users.yml \
-        --boot-cmd /fsloader/run_sgadmin.sh
+          --boot-cmd /fsloader/run_sgadmin.sh
       fi
-    ;;
+      ;;
 
     *) ;;
   esac
 fi
 echo "Ignore running sgadmin..."
-tail -f /fsloader/run_sgadmin.sh
+exec tail -f /fsloader/run_sgadmin.sh
