85b7b35e796adb95dc5fd98696533a6574a1c570
iamrz1 <rezoan@appscode.com>
2019-03-01 18:19:23 +0600

    Initial PSP support for ES
    Signed-off-by: iamrz1 <rezoan@appscode.com>

--- pkg/controller/rbac.go
+++ pkg/controller/rbac.go
@@ -3,11 +3,15 @@ package controller
 import (
 	api "github.com/kubedb/apimachinery/apis/kubedb/v1alpha1"
 	core "k8s.io/api/core/v1"
+	policy_v1beta1 "k8s.io/api/policy/v1beta1"
+	rbac "k8s.io/api/rbac/v1beta1"
 	kerr "k8s.io/apimachinery/pkg/api/errors"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	clientsetscheme "k8s.io/client-go/kubernetes/scheme"
 	"k8s.io/client-go/tools/reference"
 	core_util "kmodules.xyz/client-go/core/v1"
+	policy_util "kmodules.xyz/client-go/policy/v1beta1"
+	rbac_util "kmodules.xyz/client-go/rbac/v1beta1"
 )
 
 func (c *Controller) createServiceAccount(elasticsearch *api.Elasticsearch, saName string) error {
@@ -30,20 +34,141 @@ func (c *Controller) createServiceAccount(elasticsearch *api.Elasticsearch, saNa
 	return err
 }
 
+func (c *Controller) ensureRole(elasticsearch *api.Elasticsearch) error {
+	ref, rerr := reference.GetReference(clientsetscheme.Scheme, elasticsearch)
+	if rerr != nil {
+		return rerr
+	}
+
+	// Create new Roles
+	_, _, err := rbac_util.CreateOrPatchRole(
+		c.Client,
+		metav1.ObjectMeta{
+			Name:      elasticsearch.OffshootName(),
+			Namespace: elasticsearch.Namespace,
+		},
+		func(in *rbac.Role) *rbac.Role {
+			core_util.EnsureOwnerReference(&in.ObjectMeta, ref)
+			in.Rules = []rbac.PolicyRule{
+				{
+					APIGroups:     []string{policy_v1beta1.GroupName},
+					Resources:     []string{"podsecuritypolicies"},
+					Verbs:         []string{"use"},
+					ResourceNames: []string{elasticsearch.OffshootName()},
+				},
+			}
+			return in
+		},
+	)
+	return err
+}
+
+func (c *Controller) createRoleBinding(elasticsearch *api.Elasticsearch) error {
+	ref, rerr := reference.GetReference(clientsetscheme.Scheme, elasticsearch)
+	if rerr != nil {
+		return rerr
+	}
+	// Ensure new RoleBindings
+	_, _, err := rbac_util.CreateOrPatchRoleBinding(
+		c.Client,
+		metav1.ObjectMeta{
+			Name:      elasticsearch.OffshootName(),
+			Namespace: elasticsearch.Namespace,
+		},
+		func(in *rbac.RoleBinding) *rbac.RoleBinding {
+			core_util.EnsureOwnerReference(&in.ObjectMeta, ref)
+			in.RoleRef = rbac.RoleRef{
+				APIGroup: rbac.GroupName,
+				Kind:     "Role",
+				Name:     elasticsearch.OffshootName(),
+			}
+			in.Subjects = []rbac.Subject{
+				{
+					Kind:      rbac.ServiceAccountKind,
+					Name:      elasticsearch.OffshootName(),
+					Namespace: elasticsearch.Namespace,
+				},
+			}
+			return in
+		},
+	)
+	return err
+}
+
+func (c *Controller) ensurePSP(elasticsearch *api.Elasticsearch) error {
+	ref, rerr := reference.GetReference(clientsetscheme.Scheme, elasticsearch)
+	if rerr != nil {
+		return rerr
+	}
+
+	// Ensure Pod Security policy for Elasticsearch resources
+	escalate := true
+	_, _, err := policy_util.CreateOrPatchPodSecurityPolicy(c.Client,
+		metav1.ObjectMeta{
+			Name: elasticsearch.OffshootName(),
+		},
+		func(in *policy_v1beta1.PodSecurityPolicy) *policy_v1beta1.PodSecurityPolicy {
+			//TODO: possible function EnsureOwnerReference(&psp.ObjectMeta, ref) in kutil for non namespaced resources.
+			in.OwnerReferences = []metav1.OwnerReference{
+				{
+					APIVersion: ref.APIVersion,
+					Kind:       ref.Kind,
+					Name:       ref.Name,
+					UID:        ref.UID,
+				},
+			}
+			in.Spec = policy_v1beta1.PodSecurityPolicySpec{
+				Privileged:               false,
+				AllowPrivilegeEscalation: &escalate,
+				Volumes: []policy_v1beta1.FSType{
+					policy_v1beta1.All,
+				},
+				HostIPC:     false,
+				HostNetwork: false,
+				HostPID:     false,
+				RunAsUser: policy_v1beta1.RunAsUserStrategyOptions{
+					Rule: policy_v1beta1.RunAsUserStrategyRunAsAny,
+				},
+				SELinux: policy_v1beta1.SELinuxStrategyOptions{
+					Rule: policy_v1beta1.SELinuxStrategyRunAsAny,
+				},
+				FSGroup: policy_v1beta1.FSGroupStrategyOptions{
+					Rule: policy_v1beta1.FSGroupStrategyRunAsAny,
+				},
+				SupplementalGroups: policy_v1beta1.SupplementalGroupsStrategyOptions{
+					Rule: policy_v1beta1.SupplementalGroupsStrategyRunAsAny,
+				},
+				AllowedCapabilities: []core.Capability{
+					"IPC_LOCK",
+					"SYS_RESOURCE",
+				},
+			}
+			return in
+		},
+	)
+	return err
+}
+
 func (c *Controller) ensureRBACStuff(elasticsearch *api.Elasticsearch) error {
+	//Create PSP
+	if err := c.ensurePSP(elasticsearch); err != nil {
+		return err
+	}
+
+	// Create New Role
+	if err := c.ensureRole(elasticsearch); err != nil {
+		return err
+	}
+
+	// Create New RoleBinding
+	if err := c.createRoleBinding(elasticsearch); err != nil {
+		return err
+	}
 	// Create New ServiceAccount
 	if err := c.createServiceAccount(elasticsearch, elasticsearch.OffshootName()); err != nil {
 		if !kerr.IsAlreadyExists(err) {
 			return err
 		}
 	}
-
-	// Create New SNapshot ServiceAccount
-	if err := c.createServiceAccount(elasticsearch, elasticsearch.SnapshotSAName()); err != nil {
-		if !kerr.IsAlreadyExists(err) {
-			return err
-		}
-	}
-
 	return nil
 }
