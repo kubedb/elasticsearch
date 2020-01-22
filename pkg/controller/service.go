5124c9e5bdfb04a7e34adfb8dbe0d16766d6f122
kamolhasan <kamol@appscode.com>
2020-01-22 16:07:18 +0600

    Remove wrapper function
    Signed-off-by: kamolhasan <kamol@appscode.com>

--- pkg/controller/service.go
+++ pkg/controller/service.go
@@ -53,45 +53,36 @@ var (
 func (c *Controller) ensureElasticGvrSvc(elasticsearch *api.Elasticsearch) error {
 	owner := metav1.NewControllerRef(elasticsearch, api.SchemeGroupVersion.WithKind(api.ResourceKindElasticsearch))
 
-	svcFunc := func(svcName string, labels, selectors map[string]string) error {
-
-		// Check if service name exists with different db kind
-		if err := c.checkService(elasticsearch, svcName); err != nil {
-			return err
-		}
-
-		meta := metav1.ObjectMeta{
-			Name:      svcName,
-			Namespace: elasticsearch.Namespace,
-		}
-
-		_, vt, err := core_util.CreateOrPatchService(c.Client, meta, func(in *core.Service) *core.Service {
-			core_util.EnsureOwnerReference(&in.ObjectMeta, owner)
-			in.Labels = labels
-			in.Spec.Selector = selectors
-			in.Spec.Type = core.ServiceTypeClusterIP
-			in.Spec.ClusterIP = core.ClusterIPNone
-			in.Spec.Ports = []core.ServicePort{defaultPeerPort, defaultClientPort}
-			return in
-		})
-
-		if err == nil {
-			c.recorder.Eventf(
-				elasticsearch,
-				core.EventTypeNormal,
-				eventer.EventReasonSuccessful,
-				"Successfully %s governing service",
-				vt,
-			)
-		}
+	// Check if service name exists with different db kind
+	if err := c.checkService(elasticsearch, elasticsearch.GvrSvcName()); err != nil {
 		return err
 	}
 
-	// create elasticsearch governing service
-	return svcFunc(elasticsearch.GvrSvcName(),
-		elasticsearch.OffshootLabels(),
-		elasticsearch.OffshootSelectors(),
-	)
+	meta := metav1.ObjectMeta{
+		Name:      elasticsearch.GvrSvcName(),
+		Namespace: elasticsearch.Namespace,
+	}
+
+	_, vt, err := core_util.CreateOrPatchService(c.Client, meta, func(in *core.Service) *core.Service {
+		core_util.EnsureOwnerReference(&in.ObjectMeta, owner)
+		in.Labels = elasticsearch.OffshootLabels()
+		in.Spec.Selector = elasticsearch.OffshootSelectors()
+		in.Spec.Type = core.ServiceTypeClusterIP
+		in.Spec.ClusterIP = core.ClusterIPNone
+		in.Spec.Ports = []core.ServicePort{defaultPeerPort, defaultClientPort}
+		return in
+	})
+
+	if err == nil {
+		c.recorder.Eventf(
+			elasticsearch,
+			core.EventTypeNormal,
+			eventer.EventReasonSuccessful,
+			"Successfully %s governing service",
+			vt,
+		)
+	}
+	return err
 }
 
 func (c *Controller) ensureService(elasticsearch *api.Elasticsearch) (kutil.VerbType, error) {
